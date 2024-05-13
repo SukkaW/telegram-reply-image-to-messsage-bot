5cd9a6b1b7f662a0222f0e18a63697b5ac8e0637
SukkaW <isukkaw@gmail.com>
2024-05-13 23:38:46 +0800

    docs: add example config & readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# Telegram Reply Image to Message Bot
+
+This is a Telegram Bot that replies to a message contains a specific keyword with a specified image.
---
+++ bot.config.example.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  botToken: '',
+  allowedGroupIds: [-114514, -1919810],
+  messageImageMap: {
+    '/200': 'https://http.cat/200',
+    '/404': 'https://http.cat/404',
+    '/502': 'https://http.cat/502'
+  }
+};
